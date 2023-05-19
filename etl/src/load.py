5ae50a755f405d1c5b9c3a789f9537174a803ddc
cvillafraz <cirov@protonmail.com>
2023-05-18 20:28:11 -0400

    Handle the case where document is empty

--- etl/src/load.py
+++ etl/src/load.py
@@ -7,25 +7,29 @@ from src.transform import convert_file, preprocess_doc
 from haystack.document_stores import ElasticsearchDocumentStore
 
 
-def index_doc(pdf_doc: Dict[str, str]):
+def index_doc(pdf_doc: Dict[str, str]) -> bool:
     """Process and index a pdf document into elasticsearch
 
     Args:
         pdf_doc (Dict[str, str]): Document to index
     """
     doc = convert_file(pdf_doc["path"])
-    doc.meta["company"] = pdf_doc["company"]
-    doc.meta["year"] = pdf_doc["year"]
-    doc = preprocess_doc(doc)
-    document_store = ElasticsearchDocumentStore(
-        host=os.environ.get("ELASTICSEARCH_HOST", "localhost"),
-        username="",
-        password="",
-        index="document",
-    )
-    document_store.delete_documents()
-    logging.info(f"Indexing {len(doc)} documents")
-    document_store.write_documents(doc)
+    if doc:
+        doc.meta["company"] = pdf_doc["company"]
+        doc.meta["year"] = pdf_doc["year"]
+        doc = preprocess_doc(doc)
+        document_store = ElasticsearchDocumentStore(
+            host=os.environ.get("ELASTICSEARCH_HOST", "localhost"),
+            username="",
+            password="",
+            index="document",
+        )
+        document_store.delete_documents()
+        logging.info(f"Indexing {len(doc)} documents")
+        document_store.write_documents(doc)
+        return True
+    else:
+        return False
 
 
 class Indexer(multiprocessing.Process):
@@ -42,8 +46,11 @@ class Indexer(multiprocessing.Process):
 
             self.is_alive = bool(pdf_doc) ^ (len(glob.glob("./dataset/**/*.pdf")) == 0)
             if pdf_doc:
-                index_doc(pdf_doc)
-                logging.info(f'Indexed document with path: {pdf_doc["path"]}')
+                doc_indexed = index_doc(pdf_doc)
+                if doc_indexed:
+                    logging.info(
+                        f'Indexed processed document with path: {pdf_doc["path"]}'
+                    )
         else:
             self.connection.close()
             logging.info("Conectionn closed")
--- etl/src/transform.py
+++ etl/src/transform.py
@@ -1,27 +1,31 @@
-from typing import List
+from typing import List, Union
 from haystack.nodes import PreProcessor, PDFToTextConverter
 from src.utils.text_normalizer import normalize_text
 from haystack import Document
 import logging
 
 
-def convert_file(path: str) -> List[Document]:
+def convert_file(path: str) -> Union[List[Document], None]:
     """Converts file in path to haystack document
 
     Args:
         path (str): path of file to convert
 
     Returns:
-        Document: Haystack document from pdd todument
+        Document: Haystack document from pdf document or None if document is empty
     """
     logging.info(f"Converting file in {path} to haystack docs")
     converter = PDFToTextConverter(valid_languages=["en"])
     pdf_doc = converter.convert(path)[0]
     pdf_doc.content = normalize_text(pdf_doc.content)
-    return pdf_doc
+    if len(pdf_doc.content) > 15:
+        return pdf_doc
+    else:
+        logging.info("Document is empty, skipping")
+        return None
 
 
-def preprocess_doc(document: Document) -> List[Document]:
+def preprocess_doc(document: List[Document]) -> List[Document]:
     """Preprocesses document for indexing
 
     Args:
