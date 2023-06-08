2046e1bf0f99508b52ec94eadd3512e96f31be7d
cvillafraz <cirov@protonmail.com>
2023-06-08 14:14:37 -0400

    Fix logo in Login

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -15,7 +15,7 @@ export const Login = () =>  {
         <section>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img className="mr-2" src="/anyone.png" alt="logo"/> 
+                    <img className="mr-2" src="https://i.ibb.co/6W2b23s/anyone.png" alt="logo"/> 
                 </a>
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
